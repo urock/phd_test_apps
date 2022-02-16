03eec0548e960cc7d31c16291eba470f723ce92b
Yury Rumyantsev <urock@fastsense.tech>
2022-02-16 18:41:15 +0300

    wip on unit test

--- diveev/NOP_clean/NOP1.pas
+++ diveev/NOP_clean/NOP1.pas
@@ -9,13 +9,33 @@ uses
   Interfaces, // this includes the LCL widgetset
   Forms, Unit1, Unit2, UnitBVGPobject, Calc3, Unit3, Unit4, Unit5, Unit6, Unit7,
   Unit8, Unit9, tachartlazaruspkg, Unit10, Unit11, Unit12, Unit13, Unit14,
-  UnitAdaptObject, Unit15;
+  UnitAdaptObject, Unit15, Grids, Classes, SysUtils, FileUtil, Controls,
+  Graphics, Dialogs, StdCtrls;
 
 {$R *.res}
 
+var
+  StringGrid2: TStringGrid;
+  i,j:integer;
+
 begin
 
-  writeln ('Hello, world.');
+  writeln ('Hello, world22 L1 = ', L1);
+
+  //StringGrid2.RowCount:=L1+1;
+  //StringGrid2.ColCount:=L1+1;
+  //
+  //for i:=1 to L1 do
+  //begin
+  //  StringGrid2.Cells[i,0]:=InttoStr(i);
+  //  StringGrid2.Cells[0,i]:=InttoStr(i);
+  //end;
+  //
+  //writeln ('1');
+  //
+  //StringGrid2.LoadFromFile('/home/urock/phd/24_NOP_461');
+  //
+  //writeln ('2');
 
   RequireDerivedFormResource := True;
   Application.Initialize;
